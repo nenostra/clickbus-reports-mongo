2d97aafca666eaa9eff5c52c8b856d0fc1840df6
nenostra <neno.24@hotmail.com>
2018-05-20 20:13:23 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://nenostra:5243562e@clickbus-shard-00-00-yo6hy.'
-  + 'mongodb.net:27017,clickbus-shard-00-01-yo6hy.mongodb.net:27017,clickbus-'
-  + 'shard-00-02-yo6hy.mongodb.net:27017/test?ssl=true&replicaSet=clickbus-shard'
-  + '-0&authSource=admin');
+mongoose.connect('');
